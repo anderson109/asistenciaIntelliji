e20c305d75fabf82c84c0698a628f1c20890b421
anderson109 <xxandersoncatxx@gmail.com>
2024-07-27 20:42:42 -0600

    commit de prueba

---
+++ src/main/java/esfe/Asistencia/controladores/homeController.java
@@ -0,0 +1,13 @@
+package esfe.Asistencia.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class homeController {
+    @RequestMapping("/")
+    public String index(){
+        return "home/index";
+    }
+}
